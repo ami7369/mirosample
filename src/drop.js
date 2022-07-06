1e45f941fa7342f981ae232adbee4c696872d4c4
ami_tsumagari <ami_tsumagari@itp.co.jp>
2022-07-07 08:48:01 +0900

    drop.js changes to sample

--- src/drop.js
+++ src/drop.js
@@ -1,14 +1,25 @@
-async function init(){
-const drop = async (e) => {
-  const { x, y, target } = e;
+// async function init(){
+// const drop = async (e) => {
+//   const { x, y, target } = e;
 
-  if (target instanceof HTMLImageElement) {
-    const image = await miro.board.createImage({ x, y, url: target.src });
-    await miro.board.viewport.zoomTo(image);
-  }
-};
+//   if (target instanceof HTMLImageElement) {
+//     const image = await miro.board.createImage({ x, y, url: target.src });
+//     await miro.board.viewport.zoomTo(image);
+//   }
+// };
 
-miro.board.ui.on("drop", drop);
+// miro.board.ui.on("drop", drop);
+// }
+async function init() {
+  // Enable the 'drop' event on the app panel. Active on 'miro-draggable' HTML elements
+  await miro.board.ui.on('drop', async ({x, y, target}) => {
+    // In this example: when the HTML element is dropped on the board, a sticky note is created
+    await miro.board.createStickyNote({
+      x,
+      y,
+      content: target.textContent,
+    });
+  });
 }
 
 init();
